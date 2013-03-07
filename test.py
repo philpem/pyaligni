d1df93c09f6a319ec5b83d995d2062d04bf60905
Philip Pemberton <philpem@philpem.me.uk>
2013-03-07 14:33:49 +0000

    make tests more extensive

--- test.py
+++ test.py
@@ -3,5 +3,11 @@ import Aligni
 if __name__ == '__main__':
 	a = Aligni.API('demo', 'oid3vLgynoy_Yl1gZkrgkLEq3J')
 
-	print a.get_contact()
-	print a.get_contact(4)
+	m = a.get_manufacturer()
+	print a.get_manufacturer(m[0].id)
+
+	v = a.get_vendor()
+	print a.get_vendor(v[0].id)
+
+	c = a.get_contact()
+	print a.get_contact(c[0].id)
